09d0d585a5797f34549c88b63010481e6613d4fb
Sayak Paul <spsayakpaul@gmail.com>
2022-06-10 12:58:30 +0530

    bumped up beam version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 NAME = "count_training_tokens"
 VERSION = "0.0.1"
 REQUIRED_PACKAGES = [
-    "apache-beam[gcp]==2.32.0",
+    "apache-beam[gcp]==2.34.0",
     "google-cloud-storage==2.2.1",
     "tokenizers==0.11.6",
     "transformers==4.17.0",
